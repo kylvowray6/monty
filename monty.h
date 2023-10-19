2082468da8ec374caf081d1e7fe81baf11d1e012
kylvowray6 <101636268+kylvowray6@users.noreply.github.com>
2023-10-19 22:15:51 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -11,13 +11,9 @@
 #include <string.h>
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * struct stack_s - doubly linked list representation
+ * Description: doubly linked list
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -28,11 +24,8 @@ typedef struct stack_s
 
 /**
  * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
